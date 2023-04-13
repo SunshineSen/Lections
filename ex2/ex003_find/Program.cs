7aa13a1424ec533c242c1c1fb29b983b230485ed
sen <kseniya2495@yandex.ru>
2023-04-13 15:42:02 +0300

    элемент с индексом

---
+++ ex2/ex003_find/Program.cs
@@ -0,0 +1,18 @@
+﻿// в массиве array из n элементов нвйти элемент массива равный find
+int[] array = { 1, 2, 3, 4, 6, 8, 4, 15, 40 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
